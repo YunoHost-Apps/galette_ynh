d12a3b3e45abadcefd281e7bf17dbbfdeba678b5
Alexandre Aubin <alexandre.aubin@cern.ch>
2015-10-29 01:46:59 +0100

    Editing for tests

--- conf/config.inc.php
+++
@@ -1,8 +0,0 @@
-<?php
-define("TYPE_DB",   "mysql"          );
-define("HOST_DB",   "localhost"      );
-define("PORT_DB",   "3306"           );
-define("USER_DB",   "USER_DB_VALUE"  );
-define("PWD_DB",    "PWD_DB_VALUE"   );
-define("NAME_DB",   "NAME_DB_VALUE"  );
-define("PREFIX_DB", ""               );
