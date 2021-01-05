4c987cd43e44c6e807564a386c4f1357e85f4dc0
ericgaspar <junk.eg@free.fr>
2021-01-05 23:31:12 +0100

    Fix

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -6,7 +6,8 @@
 
 # dependencies used by the app
 YNH_PHP_VERSION="7.3"
-php_packages="php${YNH_PHP_VERSION}-tidy php${YNH_PHP_VERSION}-intl php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-mysql php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-curl"
+
+extra_php_dependencies="php${YNH_PHP_VERSION}-tidy php${YNH_PHP_VERSION}-intl php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-mysql php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-curl"
 
 #=================================================
 # EXPERIMENTAL HELPERS
