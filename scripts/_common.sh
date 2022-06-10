2017bf1f7915f8a8d171749c2e539b1e5d30bdbe
yalh76 <yalh@yahoo.com>
2022-06-10 13:11:31 +0200

    Apply last example_ynh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,11 +5,9 @@
 #=================================================
 
 # dependencies used by the app
-YNH_PHP_VERSION="7.4"
+pkg_dependencies="php$YNH_DEFAULT_PHP_VERSION-tidy php$YNH_DEFAULT_PHP_VERSION-intl php$YNH_DEFAULT_PHP_VERSION-mbstring php$YNH_DEFAULT_PHP_VERSION-xml php$YNH_DEFAULT_PHP_VERSION-gd php$YNH_DEFAULT_PHP_VERSION-curl php$YNH_DEFAULT_PHP_VERSION-mysql"
 
-pkg_dependencies="php${YNH_PHP_VERSION}-tidy php${YNH_PHP_VERSION}-intl php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-mysql"
-
-pgsql_pkg_dependencies="postgresql php${YNH_PHP_VERSION}-pgsql"
+pgsql_pkg_dependencies="postgresql php$YNH_DEFAULT_PHP_VERSION-pgsql"
 
 #=================================================
 # PERSONAL HELPERS
