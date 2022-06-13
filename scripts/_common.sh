85bc7b7eb28aa015f55c20fef428c53b1c7e7887
yalh76 <yalh@yahoo.com>
2022-06-13 15:48:17 +0200

    upgrade to PHP7.4

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,6 +4,8 @@
 # COMMON VARIABLES
 #=================================================
 
+YNH_PHP_VERSION="7.4"
+
 # dependencies used by the app
 pkg_dependencies="php$YNH_DEFAULT_PHP_VERSION-tidy php$YNH_DEFAULT_PHP_VERSION-intl php$YNH_DEFAULT_PHP_VERSION-mbstring php$YNH_DEFAULT_PHP_VERSION-xml php$YNH_DEFAULT_PHP_VERSION-gd php$YNH_DEFAULT_PHP_VERSION-curl php$YNH_DEFAULT_PHP_VERSION-mysql"
 
