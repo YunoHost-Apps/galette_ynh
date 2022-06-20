c4c38fe3def181dd0deac8d104445a37aa891dff
yalh76 <yalh@yahoo.com>
2022-06-20 19:51:59 +0200

    PHP7.4 is required

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,10 +4,12 @@
 # COMMON VARIABLES
 #=================================================
 
-# dependencies used by the app
-pkg_dependencies="php$YNH_DEFAULT_PHP_VERSION-tidy php$YNH_DEFAULT_PHP_VERSION-intl php$YNH_DEFAULT_PHP_VERSION-mbstring php$YNH_DEFAULT_PHP_VERSION-xml php$YNH_DEFAULT_PHP_VERSION-gd php$YNH_DEFAULT_PHP_VERSION-curl php$YNH_DEFAULT_PHP_VERSION-mysql"
+YNH_PHP_VERSION="7.4"
 
-pgsql_pkg_dependencies="postgresql php$YNH_DEFAULT_PHP_VERSION-pgsql"
+# dependencies used by the app
+pkg_dependencies="php${YNH_PHP_VERSION}-tidy php${YNH_PHP_VERSION}-intl php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-mysql"
+
+pgsql_pkg_dependencies="postgresql php${YNH_PHP_VERSION}-pgsql"
 
 #=================================================
 # PERSONAL HELPERS
