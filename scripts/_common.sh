0461702c9374a99706aa8eb0c4d17dfc8e99a0a6
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2023-11-11 14:01:42 +0100

    v2

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,11 +5,11 @@
 #=================================================
 
 # dependencies used by the app
-YNH_PHP_VERSION="7.4"
+#REMOVEME? YNH_PHP_VERSION="7.4"
 
-pkg_dependencies="php${YNH_PHP_VERSION}-tidy php${YNH_PHP_VERSION}-intl php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-mysql"
+#REMOVEME? pkg_dependencies="php${YNH_PHP_VERSION}-tidy php${YNH_PHP_VERSION}-intl php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-mysql"
 
-pgsql_pkg_dependencies="postgresql php${YNH_PHP_VERSION}-pgsql"
+#REMOVEME? pgsql_pkg_dependencies="postgresql php${YNH_PHP_VERSION}-pgsql"
 
 #=================================================
 # PERSONAL HELPERS
